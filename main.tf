e99c25ffb8856a29565743e52477768dcb36a70b
akentosh <akentosh@gmail.com>
2019-09-25 18:49:23 -0400

    remove region

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@
 module "ec2_instance" {
   source  = "app.terraform.io/akentosh/ec2-instance/aws"
   version = "1.21.0"
-  region = "us-east-1"
 
   ami = "ami-0ff8a91507f77f867"
   instance_count = 1
